b328fd7f223e1e5bae61946733bb7992426901ce
Yoshio <121027656+DeleguerrCidd@users.noreply.github.com>
2025-01-10 07:27:34 +0800

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 // typing text hero
 const typed = new Typed(".typing-text", {
-  strings: ["A Beginner Programmer.", "Web Developer.", "Gamer.", "Nothing.", "..."],
+  strings: ["A Beginner Programmer.", "Web Developer.", "a Gamer.", "Nothing.", "..."],
   loop: true,
   typeSpeed: 75,
   backSpeed: 25,
